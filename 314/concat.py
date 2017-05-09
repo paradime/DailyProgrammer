f837c091f660ffddf9d197b6f97e462b75d1e94d
paradime <brw4824@rit.edu>
2017-05-09 00:38:20 -0400

    make permutations functional

--- 314/concat.py
+++ 314/concat.py
@@ -6,10 +6,8 @@ def concat_small(arr):
     return permutations(arr)[0]
 
 def permutations(arr):
-    perms = []
-    for c in itertools.permutations(arr):
-        perms.append(int(''.join(map(str, c))))
-    return sorted(perms)
+    return sorted(list(map(lambda c : int(''.join(map(str, c))), 
+        set(itertools.permutations(arr)))))
 
 def concat(string):
     return [concat_small(string.split()), concat_large(string.split())]
